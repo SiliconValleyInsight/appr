47377db792851a7baef9e696351e492a3f248414
Makar Stetsenko <makazone@gmail.com>
2017-12-12 22:32:25 +0300

    Update comment message

--- scripts/post-deploy.js
+++ scripts/post-deploy.js
@@ -1,21 +1,25 @@
-const request = require('request');
-const utils = require('./utils');
-const config = require('./config');
-const log = require('./log');
+const request = require("request");
+const utils = require("./utils");
+const config = require("./config");
+const log = require("./log");
 module.exports = function postDeploy() {
-  const expUrl = `https://expo.io/@${config.expUsername}/${utils.readPackageJSON().name}`;
+  const expUrl = `https://expo.io/@${config.expUsername}/${
+    utils.readPackageJSON().name
+  }`;
   const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${expUrl}`;
-  const issueUrl = `https://${config.githubUsername}:${config.githubToken}@api.github.com/repos/${config.githubOrg}/${config.githubRepo}/issues/${config.githubPullRequestId}/comments`;
+  const issueUrl = `https://${config.githubUsername}:${
+    config.githubToken
+  }@api.github.com/repos/${config.githubOrg}/${config.githubRepo}/issues/${
+    config.githubPullRequestId
+  }/comments`;
 
-  log('Exponent URL', expUrl);
-  log('GitHub Issue URL', issueUrl);
-  log('QR Code URL ', qrUrl);
+  log("Exponent URL", expUrl);
+  log("GitHub Issue URL", issueUrl);
+  log("QR Code URL ", qrUrl);
 
   const body = `
-  :shipit: This branch has been deployed to:
+  :rocket: This PR has been deployed to:
   ${expUrl}
-
-  Download the [Expo](https://expo.io/) app and scan this QR code to get started!
 
   ![QR Code](${qrUrl})
   `;
@@ -23,16 +27,24 @@ module.exports = function postDeploy() {
   request.post(
     {
       url: issueUrl,
-      headers: { 'User-Agent': 'ci' },
+      headers: { "User-Agent": "ci" },
       body: JSON.stringify({ body })
     },
     (error, response) => {
       if (error) {
-        console.error('Failed to post comment to GitHub, an error occurred', error);
+        console.error(
+          "Failed to post comment to GitHub, an error occurred",
+          error
+        );
       } else if (response.statusCode >= 400) {
-        console.error('Failed to post comment to GitHub, request failed with', response);
+        console.error(
+          "Failed to post comment to GitHub, request failed with",
+          response
+        );
       } else {
-        console.log(`Posted message to GitHub PR #${config.githubPullRequestId}`);
+        console.log(
+          `Posted message to GitHub PR #${config.githubPullRequestId}`
+        );
       }
     }
   );
