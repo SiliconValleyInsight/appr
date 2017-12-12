f8b87e525bc0055813e2494821ecf0fbffe970fd
Makar Stetsenko <makazone@gmail.com>
2017-12-12 13:33:03 +0300

    Replace config file name

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,23 +1,25 @@
-const fs = require('fs');
+const fs = require("fs");
+
+const CONFIG_FILE_NAME_PATH = "./exp.json";
 
 function getExpPublishName(packageName, branchName) {
-  return `${packageName}-${branchName}`.replace(/[^a-zA-Z0-9\\-]/, '-');
+  return `${packageName}-${branchName}`.replace(/[^a-zA-Z0-9\\-]/, "-");
 }
 
 function readPackageJSON() {
-  return JSON.parse(fs.readFileSync('./package.json'));
+  return JSON.parse(fs.readFileSync("./package.json"));
 }
 
 function writePackageJSON(content) {
-  fs.writeFileSync('./package.json', JSON.stringify(content, null, 2));
+  fs.writeFileSync("./package.json", JSON.stringify(content, null, 2));
 }
 
 function readAppJSON() {
-  return JSON.parse(fs.readFileSync('./app.json'));
+  return JSON.parse(fs.readFileSync(CONFIG_FILE_NAME_PATH));
 }
 
 function writeAppJSON(content) {
-  fs.writeFileSync('./app.json', JSON.stringify(content, null, 2));
+  fs.writeFileSync(CONFIG_FILE_NAME_PATH, JSON.stringify(content, null, 2));
 }
 
 module.exports = {
