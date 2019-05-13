309b334dbc785fb591be3469e308ebd0410845b1
Makar Stetsenko <makazone@gmail.com>
2019-05-13 14:48:46 +0300

    fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,50 @@
-const fs = require("fs");
-const config = require('./config');
+// const fs = require("fs");
+// const config = require('./config');
 
-const CONFIG_FILE_NAME_PATH = process.env.EXPO_CONFIG_FILE_PATH || "./app.json";
+// const CONFIG_FILE_NAME_PATH = process.env.EXPO_CONFIG_FILE_PATH || "./app.json";
 
-// function getExpPublishName(packageName, branchName) {
-//   return `${packageName}-${branchName}`.replace(/[^a-zA-Z0-9\\-]/g, "-");
+// // function getExpPublishName(packageName, branchName) {
+// //   return `${packageName}-${branchName}`.replace(/[^a-zA-Z0-9\\-]/g, "-");
+// // }
+
+// function getExpPublishName(packageName, githubPullRequestId) {
+//   return `${packageName}-pr-${githubPullRequestId}`;
+
+// function getSafeName(name) {
+//   return `${name}`.replace(/[^a-zA-Z0-9\\-]/, '-');
 // }
 
-function getExpPublishName(packageName, githubPullRequestId) {
-  return `${packageName}-pr-${githubPullRequestId}`;
+// function getExpChannelName() {
+//   return getSafeName(config.expReleaseChannel);
+// }
+
+// function readPackageJSON() {
+//   return JSON.parse(fs.readFileSync("./package.json"));
+// }
+
+// function writePackageJSON(content) {
+//   fs.writeFileSync("./package.json", JSON.stringify(content, null, 2));
+// }
+
+// function readAppJSON() {
+//   return JSON.parse(fs.readFileSync(CONFIG_FILE_NAME_PATH));
+// }
+
+// function writeAppJSON(content) {
+//   fs.writeFileSync(CONFIG_FILE_NAME_PATH, JSON.stringify(content, null, 2));
+// }
+
+// module.exports = {
+//   getSafeName,
+//   getExpChannelName,
+//   readPackageJSON,
+//   writePackageJSON,
+//   readAppJSON,
+//   writeAppJSON
+// };
+
+const fs = require('fs');
+const config = require('./config');
 
 function getSafeName(name) {
   return `${name}`.replace(/[^a-zA-Z0-9\\-]/, '-');
@@ -19,19 +55,19 @@ function getExpChannelName() {
 }
 
 function readPackageJSON() {
-  return JSON.parse(fs.readFileSync("./package.json"));
+  return JSON.parse(fs.readFileSync('./package.json'));
 }
 
 function writePackageJSON(content) {
-  fs.writeFileSync("./package.json", JSON.stringify(content, null, 2));
+  fs.writeFileSync('./package.json', JSON.stringify(content, null, 2));
 }
 
 function readAppJSON() {
-  return JSON.parse(fs.readFileSync(CONFIG_FILE_NAME_PATH));
+  return JSON.parse(fs.readFileSync('./app.json'));
 }
 
 function writeAppJSON(content) {
-  fs.writeFileSync(CONFIG_FILE_NAME_PATH, JSON.stringify(content, null, 2));
+  fs.writeFileSync('./app.json', JSON.stringify(content, null, 2));
 }
 
 module.exports = {
