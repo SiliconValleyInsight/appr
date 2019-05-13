674a7ba6466532455f5e8e97241c0838255a8d9a
Makar Stetsenko <makazone@gmail.com>
2019-05-13 15:36:51 +0300

    Use non interactive flag

--- index.js
+++ index.js
@@ -7,23 +7,27 @@ const preDeploy = require('./scripts/pre-deploy');
 const postDeploy = require('./scripts/post-deploy');
 const localExp = './node_modules/expo-cli/bin/expo.js';
 log('Logging into Expo...');
-spawn(localExp, ['login', '-u', config.expUsername, '-p', config.expPassword, '--non-interactive'], loginError => {
-  if (loginError) {
-    throw new Error('Failed to log into Expo');
-  } else {
-    log('Logged into Expo.');
-    log('Preparing project for publish...');
-    preDeploy();
-  }
-
-  log('Publishing project into Expo.');
-  spawn(localExp, ['publish'], publishError => {
-    if (publishError) {
-      throw new Error('Failed to publish package to Expo');
+spawn(
+  localExp,
+  ['login', '-u', config.expUsername, '-p', config.expPassword, '--non-interactive'],
+  loginError => {
+    if (loginError) {
+      throw new Error('Failed to log into Expo');
     } else {
-      log('Published project.');
-      log('Notifying GitHub...');
-      postDeploy();
+      log('Logged into Expo.');
+      log('Preparing project for publish...');
+      preDeploy();
     }
-  });
-});
+
+    log('Publishing project into Expo.');
+    spawn(localExp, ['publish', '--non-interactive'], publishError => {
+      if (publishError) {
+        throw new Error('Failed to publish package to Expo');
+      } else {
+        log('Published project.');
+        log('Notifying GitHub...');
+        postDeploy();
+      }
+    });
+  }
+);
